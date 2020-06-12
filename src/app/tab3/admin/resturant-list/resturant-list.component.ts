5b71dd9f92fbbc94ef3bf618d7cf69ca93ac557b
SakBran <saobran.aung61@gmail.com>
2020-06-12 14:04:59 +0630

    commit

--- src/app/tab3/admin/resturant-list/resturant-list.component.ts
+++ src/app/tab3/admin/resturant-list/resturant-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-resturant-list',
@@ -7,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResturantListComponent implements OnInit {
 
-  constructor() { }
+  constructor(public location:Location) { }
 
   ngOnInit() {}
-
+  back(){
+    this.location.back();
+  }
 }
