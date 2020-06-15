691cc0ce23c1c18eaa5e21c84b368b1e1277c549
Sak Bran Aung <saobran.aung61@gmail.com>
2020-06-15 18:41:10 +0630

    commit on master

--- src/app/tab3/admin/user-create/user-create.component.ts
+++ src/app/tab3/admin/user-create/user-create.component.ts
@@ -8,10 +8,32 @@ import { Location } from '@angular/common';
 })
 export class UserCreateComponent implements OnInit {
 
-  constructor(public location:Location) { }
-
+    if(this.id!==null|| this.id!==undefined || this.id!==0){
+      this.editLoad(this.id);
+    }
+  }
+  userData: userModel = {
+    id: 0,
+    username: "",
+    password: "",
+    phone: "",
+    usertype: 0,
+    latitude: string;
+    longitude: string;
+  };
   ngOnInit() {}
   back(){
     this.location.back();
   }
 }
+  formValidation(): boolean {
+    if (
+      this.userData.password === "" ||
+      this.userData.phone === "" ||
+      this.userData.username === "" ||
+      this.userData.usertype === 0
+    ) {
+      return false;
+    }
+    return true;
+  }
