01b6d98a32f238411c325b891be9f7b22ccc1461
Sak Bran Aung <33890377+SakBran@users.noreply.github.com>
2020-08-25 10:41:24 +0630

    change login

--- src/app/login/login-from/login-from.component.ts
+++ src/app/login/login-from/login-from.component.ts
@@ -1,4 +1,4 @@
-import { userModel } from './../../Models/userModel';
+import { userModel } from "./../../Models/userModel";
 import { appSetting } from "src/app/app-setting";
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
@@ -12,14 +12,11 @@ import { UserTypeService } from "src/app/Services/userType/user-type.service";
 })
 export class LoginFromComponent implements OnInit {
   constructor(
-
     private router: Router,
     private userService: UserModelService,
     private appSetting: appSetting,
     private userTypeService: UserTypeService
-  ) {
-   
-  }
+  ) {}
 
   ngOnInit() {}
   username: string = "";
@@ -42,27 +39,25 @@ export class LoginFromComponent implements OnInit {
     latitude: "",
     longitude: "",
   };
-  locationUpdate(){
+  locationUpdate() {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(position => {
+      navigator.geolocation.getCurrentPosition((position) => {
         this.userData.latitude = position.coords.latitude.toString();
         this.userData.longitude = position.coords.longitude.toString();
-        
+
         this.userService.putLocation(this.userData);
       });
     }
   }
   onClick() {
     this.appSetting.showLoading();
-  
 
     this.userService.getLogin(this.username, this.password).subscribe(
       (x) => {
-    
         this.appSetting.sessionUserID = x.id;
         this.userData = x;
       },
-      (err) => this.appSetting.showError(err),
+      (err) => this.appSetting.loginFail(),
       () => {
         if (this.appSetting.sessionUserID === 0) {
           this.appSetting.loginFail();
@@ -70,26 +65,21 @@ export class LoginFromComponent implements OnInit {
           this.userTypeService.getSingle(this.userData.usertype).subscribe(
             (y) => {
               this.appSetting.loginType = y.usertypeName.toLocaleLowerCase();
-              
             },
             (err) => this.appSetting.showError(err),
             () => {
               if (this.appSetting.loginType === "resturant") {
                 this.appSetting.resturantID = this.appSetting.sessionUserID;
               }
-          
-              
+
               if (this.appSetting.loginType === "rider") {
                 setInterval(() => {
                   //this.locationUpdate();
-               this.locationUpdate();
-              
-                 
+                  this.locationUpdate();
                 }, 10000);
               }
               this.appSetting.loginSuccess();
               this.router.navigateByUrl("/managmenet");
-
             }
           );
         }
