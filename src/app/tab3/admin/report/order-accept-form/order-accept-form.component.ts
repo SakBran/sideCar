ed7593031c059c8da633d50eb45456367f7ea944
Sak Bran Aung <33890377+SakBran@users.noreply.github.com>
2020-10-05 11:59:46 +0630

    Create order Accept Report View

---
+++ src/app/tab3/admin/report/order-accept-form/order-accept-form.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order-accept-form',
+  templateUrl: './order-accept-form.component.html',
+  styleUrls: ['./order-accept-form.component.scss'],
+})
+export class OrderAcceptFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
