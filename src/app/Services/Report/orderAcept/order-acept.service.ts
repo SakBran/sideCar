03f0b6f20a5da0cc6b34342d6a929c23b1a98403
Sak Bran Aung <33890377+SakBran@users.noreply.github.com>
2020-10-05 12:10:33 +0630

    Order Acept Service

--- src/app/Services/Report/orderAcept/order-acept.service.ts
+++ src/app/Services/Report/orderAcept/order-acept.service.ts
@@ -1,9 +1,27 @@
+import { orderAcceptModel } from './../../../Models/orderAccpetModel';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { appSetting } from 'src/app/app-setting';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderAceptService {
+  private url = `${this.appSetting.apiAddress}/api/OrderAcceptanceForm`;
+  private httpOptions = {
+    headers: new HttpHeaders({
+      "Content-Type": "application/json",
+    }),
+  };
+  constructor(
+    private http: HttpClient,
+    private appSetting: appSetting
+  ) {}
 
-  constructor() { }
+  get(start:Date,end:Date): Observable<orderAcceptModel[]> {
+    return this.http.get<orderAcceptModel[]>(
+      this.url + `/resturant?start=${start}&end=${end}`
+    );
+  }
 }
